5fd9c395568554ad300fce09fd409dcef92cc7e1
lihf <lihf@genvict.com>
2020-03-27 17:19:30 +0800

    add unique_prt test source file

---
+++ unique_ptr_test/unique_ptr_test.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+class demo
+{
+public:
+    demo(int setting):m_setting(setting)
+    {
+        cout << "Create demo object resource" << endl;
+    }
+
+    ~demo()
+    {
+        cout << "Destroy demo object resource" << endl;
+    }
+
+    int m_setting;
+};
+
+int main()
+{   //测试使用智能指针pdmo，去管理堆中分配的demo对象资源，在函数结束后自动释放
+    std::cout << "Hello World! Start our test!\n";
+
+    unique_ptr<demo> pdmo(new demo(50));
+    cout << "demo.m_setting:" << pdmo->m_setting << endl;
+    return 0;
+}
